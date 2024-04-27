82e466070744828cfb3e931b98344877984212e5
goropikari <goropikari56@gmail.com>
2024-04-27 13:04:58 +0900

    paste current buffer

--- lua/termitary-mod.lua
+++ lua/termitary-mod.lua
@@ -6,13 +6,13 @@ M.state = {
 
 -- Use the current or create a new terminal buffer
 local function activate()
-  if M.state.terminal_id and vim.api.nvim_buf_is_valid(M.state.terminal_id) then
+  if M.state.terminal_id ~= vim.bo.channel then
     M.state.terminal_id = nil
   end
   if not M.state.terminal_id then
     vim.api.nvim_command('terminal')
     vim.api.nvim_feedkeys('G', 't', false)
-    M.state.terminal_id = vim.b.terminal_job_id
+    M.state.terminal_id = vim.bo.channel
   end
 end
 
